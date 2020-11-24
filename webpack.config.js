41ee25ba40ec3b1bad62e06069c180fbde1d197d
zaretmir <nazaretmiranda@gmail.com>
2020-11-24 15:41:08 +0000

    :wrench: Configure CSS webpack pipe.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    entry: ['./src/index.js', './src/styles.css'],
     output: {
         filename: "[name].[chunkhash].js",
         path: path.resolve(process.cwd(), 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            }
+        ],
+    },
     plugins: [
+        new MiniCssExtractPlugin(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             filename: "index.html",
