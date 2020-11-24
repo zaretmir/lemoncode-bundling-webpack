40a5dbd416f0ca25961551e0241af2ecdf884e8a
zaretmir <nazaretmiranda@gmail.com>
2020-11-24 14:18:33 +0000

    :wrench: Use chunkhash and configure clean-webpack-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+    output: {
+        filename: "[name].[chunkhash].js",
+        path: path.resolve(process.cwd(), 'dist'),
+    },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             filename: "index.html",
             template: "./src/index.html"
