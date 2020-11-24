3ca93ac3ee53359ee5eed7885ccb38df9b102928
zaretmir <nazaretmiranda@gmail.com>
2020-11-24 14:05:19 +0000

    :wrench: Config html-webpack-plugin.

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html"
+        }),
+    ]
+}
